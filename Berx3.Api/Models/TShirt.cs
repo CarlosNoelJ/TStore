4896c043847fe4a7cc71e1e37ccb736e030563b9
Ice-Blcker <carlosnoelg3@outlook.com>
2025-03-13 02:05:35 -0500

    Add Models with their attributes

---
+++ Berx3.Api/Models/TShirt.cs
@@ -0,0 +1,11 @@
+﻿namespace Berx3.Api.Models
+{
+    public class TShirt
+    {
+        public int Id { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public string Size { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
