6b310699b33748a7d1688258b29c09a64a6e604f
Ice-Blcker <carlosnoelg3@outlook.com>
2025-03-13 02:05:35 -0500

    Add Models with their attributes

---
+++ Berx3.Api/Models/Purchase.cs
@@ -0,0 +1,15 @@
+﻿namespace Berx3.Api.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public int TShirtId { get; set; }
+        public TShirt? TShirt { get; set; }
+
+        public int Quantity { get; set; }
+        public DateTime PurchaseDate { get; set; } = DateTime.UtcNow;
+    }
+}
