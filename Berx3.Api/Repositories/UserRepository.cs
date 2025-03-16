5e75f1fbb86de8103f3899d04ac492f70b40c187
Carlos Noel <cnoel@bicsa.onmicrosoft.com>
2025-03-16 16:41:56 -0500

    User Controller added

---
+++ Berx3.Api/Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using Berx3.Api.Data;
+using Berx3.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Berx3.Api.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+            => _context = context;
+
+        public async Task AddAsync(User entity)
+        {
+            await _context.Users.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+            
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+            => await _context.Users.ToListAsync();
+
+        public async Task<User?> GetByIdAsync(int id)
+            => await _context.Users.FindAsync(id);
+
+        public async Task SoftDeleteAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null) { 
+                user.IsActive = false;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateAsync(User entity)
+        {
+            _context.Users.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
