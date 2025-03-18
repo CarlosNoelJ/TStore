e3c9bfe1382281e94f5eca9d5fe4eef1be7fc39b
PapiArca <noelalonsora@gmail.com>
2025-03-18 18:48:35 -0500

    TShirts Controller Added

---
+++ Berx3.Api/Repositories/TshirtRepository.cs
@@ -0,0 +1,51 @@
+﻿using Berx3.Api.Data;
+using Berx3.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Berx3.Api.Repositories
+{
+    public class TshirtRepository : IRepository<TShirt>
+    {
+
+        private readonly AppDbContext _context;
+
+        public TshirtRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TShirt>> GetAllAsync()
+        {
+            return await _context.TShirts
+                .Where(t => t.IsAvailable)
+                .ToListAsync();
+        }
+
+        public async Task<TShirt?> GetByIdAsync(int id)
+        {
+            return await _context.TShirts.FindAsync(id);
+        }
+
+        public async Task AddAsync(TShirt entity)
+        {
+            await _context.TShirts.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TShirt entity)
+        {
+            _context.TShirts.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task SoftDeleteAsync(int id)
+        {
+            var tshirt = await _context.TShirts.FindAsync(id);
+            if (tshirt != null)
+            {
+                tshirt.IsAvailable = false;
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
